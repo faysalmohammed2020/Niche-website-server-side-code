d9cd47b599595ed6e2243174098b4b0258b996ab
Your Name <you@example.com>
2021-11-11 01:55:18 +0600

    Server Connected only

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const app = express();
+//middlware
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 5000
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uxyvb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run(){
+    try{
+        await client.connect();
+        console.log("Database Successfully Connected");
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Hello Niche Website Backend!')
+})
+
+app.listen(port, () => {
+  console.log(`Listening From http://localhost:${port}`)
+})
